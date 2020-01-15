efa6833cd93d18201af268edbe4108c8b2bde02b
tjnuccio <tj_nuccio@yahoo.com>
2020-01-14 21:31:38 -0800

    Koans completed and unit tests for project 1

--- airline/src/main/java/edu/pdx/cs410J/nucciotj/Flight.java
+++ airline/src/main/java/edu/pdx/cs410J/nucciotj/Flight.java
@@ -3,14 +3,51 @@ package edu.pdx.cs410J.nucciotj;
 import edu.pdx.cs410J.AbstractFlight;
 
 public class Flight extends AbstractFlight {
+
+  private int flightNum;
+  private String flightSrc;
+  private String departTime;
+  private String departDate;
+  private String flightDest;
+  private String arrivalTime;
+  private String arrivalDate;
+
+  public Flight(int flightNum, String flightSrc, String departTime, String departDate, String flightDest, String arrivalTime, String arrivalDate) {
+
+    if(flightNum < 0) {
+      throw new IllegalArgumentException("Flight number must be positive");
+    }
+
+    this.flightNum = flightNum;
+
+    if(flightSrc.length() != 3) {
+      throw new IllegalArgumentException(flightSrc + " contains too many letters.");
+    }
+    else if(flightSrc.matches(".*\\d.*")) {
+      throw new IllegalArgumentException(flightSrc + " contains integer values.");
+    }
+
+    this.flightSrc = flightSrc;
+
+    if(flightDest.length() != 3) {
+      throw new IllegalArgumentException(flightDest + " contains too many letters.");
+    }
+    else if(flightDest.matches(".*\\d.*")) {
+      throw new IllegalArgumentException(flightDest + " contains integer values.");
+    }
+
+    this.flightDest = flightDest;
+
+  }
+
   @Override
   public int getNumber() {
-    return 42;
+    return this.flightNum;
   }
 
   @Override
   public String getSource() {
-    throw new UnsupportedOperationException("This method is not implemented yet");
+    return this.flightSrc;
   }
 
   @Override
@@ -20,7 +57,7 @@ public class Flight extends AbstractFlight {
 
   @Override
   public String getDestination() {
-    throw new UnsupportedOperationException("This method is not implemented yet");
+    return this.flightDest;
   }
 
   @Override
--- airline/src/test/java/edu/pdx/cs410J/nucciotj/FlightTest.java
+++ airline/src/test/java/edu/pdx/cs410J/nucciotj/FlightTest.java
@@ -6,26 +6,71 @@ import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 /**
- * Unit tests for the {@link Flight} class.
+ * Unit testgit s for the {@link Flight} class.
  */
 public class FlightTest {
-  
-  @Test(expected = UnsupportedOperationException.class)
-  public void getArrivalStringNeedsToBeImplemented() {
-    Flight flight = new Flight();
-    flight.getArrivalString();
+
+  @Test
+  public void getFlightNumEqualsTen() {
+    int flightNum = 10;
+    Flight flight = new Flight(flightNum, "AAA", "doesn't matter", "doesn't matter", "AAA", "doesn't matter", "doesn't matter");
+    assertThat(flightNum, equalTo(flight.getNumber()));
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void whenFlightNumberLessThanZeroThrowIllegalArgumentException() {
+    int flightNum = -1;
+    Flight flight = new Flight(flightNum, "AAA", "doesn't matter", "doesn't matter", "AAA", "doesn't matter", "doesn't matter");
   }
 
   @Test
-  public void initiallyAllFlightsHaveTheSameNumber() {
-    Flight flight = new Flight();
-    assertThat(flight.getNumber(), equalTo(42));
+  public void getFlightSourceWhenSetToAAA() {
+    String src = "AAA";
+    Flight flight = new Flight(1, src, "AAA", "doesn't matter", "AAA", "doesn't matter", "doesn't matter");
+    assertThat(flight.getSource(), equalTo(src));
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void whenFlightSrcContainsMoreThan3DigitsThrowIllegalArgumentException() {
+    String src = "AAAA";
+    Flight flight = new Flight(1, src, "doesn't matter", "doesn't matter", "AAA", "doesn't matter", "doesn't matter");
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void whenFlightSrcContainsLessThan3DigitsThrowIllegalArgumentException() {
+    String src = "AA";
+    Flight flight = new Flight(1, src, "doesn't matter", "doesn't matter", "AAA", "doesn't matter", "doesn't matter");
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void whenFlightSourceContainsADigitThrowsIllegalArgumentException() {
+    String src = "AA1";
+    Flight flight = new Flight(1, src, "doesn't matter", "doesn't matter", "AAA", "doesn't matter", "doesn't matter");
   }
 
   @Test
-  public void forProject1ItIsOkayIfGetDepartureTimeReturnsNull() {
-    Flight flight = new Flight();
-    assertThat(flight.getDeparture(), is(nullValue()));
+  public void getFlightDestWhenSetToAAA() {
+    String dest = "AAA";
+    Flight flight = new Flight(1, "AAA", "doesn't matter", "doesn't matter", dest, "doesn't matter", "doesn't matter");
+    assertThat(flight.getSource(), equalTo(dest));
   }
-  
+
+  @Test(expected = IllegalArgumentException.class)
+  public void whenFlightDestContainsMoreThan3DigitsThrowIllegalArgumentException() {
+    String dest = "AAAA";
+    Flight flight = new Flight(1, "AAA", "doesn't matter", "doesn't matter", dest, "doesn't matter", "doesn't matter");
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void whenFlightDestContainsLessThan3DigitsThrowIllegalArgumentException() {
+    String dest = "AA";
+    Flight flight = new Flight(1, "AAA", "doesn't matter", "doesn't matter", dest, "doesn't matter", "doesn't matter");
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void whenFlightDestContainsADigitThrowsIllegalArgumentException() {
+    String dest = "AA1";
+    Flight flight = new Flight(1, "AAA", "doesn't matter", "doesn't matter", dest, "doesn't matter", "doesn't matter");
+  }
+
 }
