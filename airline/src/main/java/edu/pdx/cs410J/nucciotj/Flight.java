2ac4858795ec99f04f26c3dddc3429bda7f29290
tjnuccio <tj_nuccio@yahoo.com>
2020-01-16 00:20:42 -0800

    Added tests

--- airline/src/main/java/edu/pdx/cs410J/nucciotj/Flight.java
+++ airline/src/main/java/edu/pdx/cs410J/nucciotj/Flight.java
@@ -1,42 +1,97 @@
 package edu.pdx.cs410J.nucciotj;
 
 import edu.pdx.cs410J.AbstractFlight;
+import java.util.Date;
 
 public class Flight extends AbstractFlight {
 
   private int flightNum;
   private String flightSrc;
-  private String departTime;
-  private String departDate;
   private String flightDest;
-  private String arrivalTime;
-  private String arrivalDate;
+  private Date arrivalDate;
+  private Date departDate;
 
   public Flight(int flightNum, String flightSrc, String departTime, String departDate, String flightDest, String arrivalTime, String arrivalDate) {
 
-    if(flightNum < 0) {
+    String [] aDate;
+    String [] aTime;
+    String [] dDate;
+    String [] dTime;
+
+    if(flightNum < 0) {                                                         //Flight number
       throw new IllegalArgumentException("Flight number must be positive");
     }
+    else {
+      this.flightNum = flightNum;
+    }
 
-    this.flightNum = flightNum;
-
+    //Flight Source
     if(flightSrc.length() != 3) {
       throw new IllegalArgumentException(flightSrc + " contains too many letters.");
     }
     else if(flightSrc.matches(".*\\d.*")) {
       throw new IllegalArgumentException(flightSrc + " contains integer values.");
     }
+    else {
+      this.flightSrc = flightSrc;
+    }
 
-    this.flightSrc = flightSrc;
-
+    //Flight destination
     if(flightDest.length() != 3) {
       throw new IllegalArgumentException(flightDest + " contains too many letters.");
     }
     else if(flightDest.matches(".*\\d.*")) {
       throw new IllegalArgumentException(flightDest + " contains integer values.");
     }
+    else {
+      this.flightDest = flightDest;
+    }
 
-    this.flightDest = flightDest;
+    //Arrival date                                                                         //Flight arrival date + time
+    if(!arrivalDate.matches("\\d\\d?/\\d\\d?/\\d\\d\\d\\d")) {                      //Match input date to regex
+      throw new IllegalArgumentException(arrivalDate + " is incorrectly formatted. Should be of format: m/d/yyyy");
+    }
+    else if(!arrivalTime.matches("\\d\\d?:\\d\\d")) {                               //Match input time to regex
+      throw new IllegalArgumentException(arrivalTime + " is incorrectly formatted. Should be of format: hour:mins");
+    }
+    else {
+
+      aDate = arrivalDate.split("/");         //Arrival date string parsed into array of strings containing year - 0, month - 1, and day - 2
+      aTime = arrivalTime.split(":");         //Arrival time string parsed into array of strings containing hour - 0 and mins - 1
+
+      if(Integer.parseInt(aTime[0]) > 23 || Integer.parseInt(aTime[1]) > 59) {            //Check for hours and mins out of bounds
+        throw new IllegalArgumentException("Arrival time should formatted with hours between 0 - 23 and mins between 0 - 59.");
+      }
+      else if(Integer.parseInt(aDate[0]) > 12 || Integer.parseInt(aDate[1]) > 31) {       //Check for months and days our of bounds
+        throw new IllegalArgumentException("Arrival date should formatted with month between 1 - 12 and day between 1 - 31.");
+      }
+      else {                                                                            //Create date object
+        this.arrivalDate = new Date(Integer.parseInt(aDate[2]) - 1900, Integer.parseInt(aDate[0]) - 1, Integer.parseInt(aDate[1]), Integer.parseInt(aTime[0]), Integer.parseInt(aTime[1]));
+      }
+    }
+
+    //Departure date
+    if(!departDate.matches("\\d\\d?/\\d\\d?/\\d\\d\\d\\d")) {                      //Match input date to regex
+      throw new IllegalArgumentException(departDate + " is incorrectly formatted. Should be of format: m/d/yyyy");
+    }
+    else if(!departTime.matches("\\d\\d?:\\d\\d")) {                               //Match input time to regex
+      throw new IllegalArgumentException(departTime + " is incorrectly formatted. Should be of format: hour:mins");
+    }
+    else {
+
+      dDate = departDate.split("/");         //Departure date string parsed into array of strings containing year - 0, month - 1, and day - 2
+      dTime = departTime.split(":");         //Departure time string parsed into array of strings containing hour - 0 and mins - 1
+
+      if(Integer.parseInt(dTime[0]) > 23 || Integer.parseInt(dTime[1]) > 59) {            //Check for hours and mins out of bounds
+        throw new IllegalArgumentException("Departure time should formatted with hours between 0 - 23 and mins between 0 - 59.");
+      }
+      else if(Integer.parseInt(dDate[0]) > 12 || Integer.parseInt(dDate[1]) > 31) {       //Check for months and days our of bounds
+        throw new IllegalArgumentException("Departure date should formatted with month between 1 - 12 and day between 1 - 31.");
+      }
+      else {                                                                            //Create date object
+        this.departDate = new Date(Integer.parseInt(dDate[2]) - 1900, Integer.parseInt(dDate[0]) - 1, Integer.parseInt(dDate[1]), Integer.parseInt(dTime[0]), Integer.parseInt(dTime[1]));
+      }
+    }
 
   }
 
@@ -51,6 +106,11 @@ public class Flight extends AbstractFlight {
   }
 
   @Override
+  public String getArrivalString() {
+    throw new UnsupportedOperationException("This method is not implemented yet");
+  }
+
+  @Override
   public String getDepartureString() {
     throw new UnsupportedOperationException("This method is not implemented yet");
   }
@@ -61,7 +121,12 @@ public class Flight extends AbstractFlight {
   }
 
   @Override
-  public String getArrivalString() {
-    throw new UnsupportedOperationException("This method is not implemented yet");
+  public Date getArrival() {
+    return this.arrivalDate;
+  }
+
+  @Override
+  public Date getDeparture() {
+    return this.departDate;
   }
 }
--- airline/src/test/java/edu/pdx/cs410J/nucciotj/FlightTest.java
+++ airline/src/test/java/edu/pdx/cs410J/nucciotj/FlightTest.java
@@ -2,75 +2,240 @@ package edu.pdx.cs410J.nucciotj;
 
 import org.junit.Test;
 
+import java.util.Date;
+
 import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 /**
- * Unit testgit s for the {@link Flight} class.
+ * Unit tests for the {@link Flight} class.
  */
 public class FlightTest {
 
   @Test
   public void getFlightNumEqualsTen() {
     int flightNum = 10;
-    Flight flight = new Flight(flightNum, "AAA", "doesn't matter", "doesn't matter", "AAA", "doesn't matter", "doesn't matter");
+    Flight flight = new Flight(flightNum, "AAA", "10:00", "3/15/2017", "AAA", "11:00", "3/15/2017");
     assertThat(flightNum, equalTo(flight.getNumber()));
   }
 
   @Test(expected = IllegalArgumentException.class)
   public void whenFlightNumberLessThanZeroThrowIllegalArgumentException() {
     int flightNum = -1;
-    Flight flight = new Flight(flightNum, "AAA", "doesn't matter", "doesn't matter", "AAA", "doesn't matter", "doesn't matter");
+    Flight flight = new Flight(flightNum, "AAA", "10:00", "3/15/2017", "AAA", "11:00", "3/15/2017");
   }
 
   @Test
   public void getFlightSourceWhenSetToAAA() {
     String src = "AAA";
-    Flight flight = new Flight(1, src, "AAA", "doesn't matter", "AAA", "doesn't matter", "doesn't matter");
+    Flight flight = new Flight(1, src, "10:00", "3/15/2017", "AAA", "11:00", "3/15/2017");
     assertThat(flight.getSource(), equalTo(src));
   }
 
   @Test(expected = IllegalArgumentException.class)
-  public void whenFlightSrcContainsMoreThan3DigitsThrowIllegalArgumentException() {
+  public void whenFlightSrcContainsMoreThan3LettersThrowIllegalArgumentException() {
     String src = "AAAA";
-    Flight flight = new Flight(1, src, "doesn't matter", "doesn't matter", "AAA", "doesn't matter", "doesn't matter");
+    Flight flight = new Flight(1, src, "10:00", "3/15/2017", "AAA", "11:00", "3/15/2017");
   }
 
   @Test(expected = IllegalArgumentException.class)
-  public void whenFlightSrcContainsLessThan3DigitsThrowIllegalArgumentException() {
+  public void whenFlightSrcContainsLessThan3LettersThrowIllegalArgumentException() {
     String src = "AA";
-    Flight flight = new Flight(1, src, "doesn't matter", "doesn't matter", "AAA", "doesn't matter", "doesn't matter");
+    Flight flight = new Flight(1, src, "10:00", "3/15/2017", "AAA", "11:00", "3/15/2017");
   }
 
   @Test(expected = IllegalArgumentException.class)
   public void whenFlightSourceContainsADigitThrowsIllegalArgumentException() {
     String src = "AA1";
-    Flight flight = new Flight(1, src, "doesn't matter", "doesn't matter", "AAA", "doesn't matter", "doesn't matter");
+    Flight flight = new Flight(1, src, "10:00", "3/15/2017", "AAA", "11:00", "3/15/2017");
   }
 
   @Test
   public void getFlightDestWhenSetToAAA() {
     String dest = "AAA";
-    Flight flight = new Flight(1, "AAA", "doesn't matter", "doesn't matter", dest, "doesn't matter", "doesn't matter");
-    assertThat(flight.getSource(), equalTo(dest));
+    Flight flight = new Flight(1, "AAA", "10:00", "3/15/2017", dest, "11:00", "3/15/2017");
+    assertThat(flight.getDestination(), equalTo(dest));
   }
 
   @Test(expected = IllegalArgumentException.class)
-  public void whenFlightDestContainsMoreThan3DigitsThrowIllegalArgumentException() {
+  public void whenFlightDestContainsMoreThan3LettersThrowIllegalArgumentException() {
     String dest = "AAAA";
-    Flight flight = new Flight(1, "AAA", "doesn't matter", "doesn't matter", dest, "doesn't matter", "doesn't matter");
+    Flight flight = new Flight(1, "AAA", "10:00", "3/15/2017", dest, "11:00", "3/15/2017");
   }
 
   @Test(expected = IllegalArgumentException.class)
-  public void whenFlightDestContainsLessThan3DigitsThrowIllegalArgumentException() {
+  public void whenFlightDestContainsLessThan3LettersThrowIllegalArgumentException() {
     String dest = "AA";
-    Flight flight = new Flight(1, "AAA", "doesn't matter", "doesn't matter", dest, "doesn't matter", "doesn't matter");
+    Flight flight = new Flight(1, "AAA", "10:00", "3/15/2017", dest, "11:00", "3/15/2017");
   }
 
   @Test(expected = IllegalArgumentException.class)
   public void whenFlightDestContainsADigitThrowsIllegalArgumentException() {
     String dest = "AA1";
-    Flight flight = new Flight(1, "AAA", "doesn't matter", "doesn't matter", dest, "doesn't matter", "doesn't matter");
+    Flight flight = new Flight(1, "AAA", "10:00", "3/15/2017", dest, "11:00", "3/15/2017");
   }
 
+  @Test
+  public void getFlightArrivalForDateOfMarchFifteenthTwentySeventeenAndTimeOfElevenAM() {
+    Date date = new Date((2017 - 1900), (3 - 1), 15, 11, 00);
+    Flight flight = new Flight(1, "AAA", "10:00", "3/15/2017", "AAA", "11:00", "3/15/2017");
+    assertThat(flight.getArrival(), equalTo(date));
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void whenArrivalDateDoesNotContainTwoDashesThrowIllegalArgumentException() {
+    Flight flight = new Flight(1, "AAA", "10:00", "3/15/2017", "AAA", "11:00", "3152017");
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void whenArrivalDateDoesNotContainMonthDayYearThrowIllegalArgumentException() {
+    Flight flight = new Flight(1, "AAA", "10:00", "3/15/2017", "AAA", "11:00", "//");
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void whenArrivalDateContainsTooManyDigitsForMonthThrowIllegalArgumentException() {
+    Flight flight = new Flight(1, "AAA", "10:00", "3/15/2017", "AAA", "11:00", "333/1/2017");
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void whenArrivalDateContainsMonthOver12ThrowIllegalArgumentException() {
+    Flight flight = new Flight(1, "AAA", "10:00", "3/15/2017", "AAA", "11:00", "13/1/2017");
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void whenArrivalDateContainsDaysOver31ThrowIllegalArgumentException() {
+    Flight flight = new Flight(1, "AAA", "10:00", "3/15/2017", "AAA", "11:00", "12/32/2017");
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void whenArrivalDateContainsTooManyDigitsForDayThrowIllegalArgumentException() {
+    Flight flight = new Flight(1, "AAA", "10:00", "3/15/2017", "AAA", "11:00", "3/100/2017");
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void whenArrivalDateContainsTooManyDigitsForYearThrowIllegalArgumentException() {
+    Flight flight = new Flight(1, "AAA", "10:00", "3/15/2017", "AAA", "11:00", "3/10/20177");
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void whenArrivalDateContainsCharactersForMonthThrowException() {
+    Flight flight = new Flight(1, "AAA", "10:00", "3/15/2017", "AAA", "11:00", "March/10/20177");
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void whenArrivalTimeDoesNotContainColonThrowIllegalArgumentException() {
+    Flight flight = new Flight(1, "AAA", "10:00", "3/15/2017", "AAA", "1100", "3/15/2017");
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void whenArrivalTimeContainsTooManyDigitsForHourThrowException() {
+    Flight flight = new Flight(1, "AAA", "10:00", "3/15/2017", "AAA", "111:00", "3/15/2017");
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void whenArrivalTimeDoesNotContainHoursThrowException() {
+    Flight flight = new Flight(1, "AAA", "10:00", "3/15/2017", "AAA", ":00", "3/15/2017");
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void whenArrivalTimeDoesNotContainMinsThrowException() {
+    Flight flight = new Flight(1, "AAA", "10:00", "3/15/2017", "AAA", "11:", "3/15/2017");
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void whenArrivalTimeHasHoursGreaterThan23ThrowIllegalArgumentException() {
+    Flight flight = new Flight(1, "AAA", "10:00", "3/15/2017", "AAA", "24:00", "3/15/2017");
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void whenArrivalTimeHasMinsGreaterThan59ThrowIllegalArgumentException() {
+    Flight flight = new Flight(1, "AAA", "10:00", "3/15/2017", "AAA", "23:61", "3/15/2017");
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void whenArrivalTimeHasNeagtiveHoursThrowIllegalArgumentException() {
+    Flight flight = new Flight(1, "AAA", "10:00", "3/15/2017", "AAA", "-10:00", "3/15/2017");
+  }
+
+  @Test
+  public void getFlightDepartureForDateOfMarchFifteenthTwentySeventeenAndTimeOfElevenAM() {
+    Date date = new Date((2017 - 1900), (3 - 1), 15, 11, 00);
+    Flight flight = new Flight(1, "AAA", "11:00", "3/15/2017", "AAA", "11:00", "3/15/2017");
+    assertThat(flight.getDeparture(), equalTo(date));
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void whenDepartureDateDoesNotContainTwoDashesThrowIllegalArgumentException() {
+    Flight flight = new Flight(1, "AAA", "10:00", "3152017", "AAA", "11:00", "3/15/2017");
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void whenDepartureDateDoesNotContainMonthDayYearThrowIllegalArgumentException() {
+    Flight flight = new Flight(1, "AAA", "10:00", "//", "AAA", "11:00", "3/15/2017");
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void whenDepartureDateContainsTooManyDigitsForMonthThrowIllegalArgumentException() {
+    Flight flight = new Flight(1, "AAA", "10:00", "333/15/2017", "AAA", "11:00", "3/15/2017");
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void whenDepartureDateContainsMonthOver12ThrowIllegalArgumentException() {
+    Flight flight = new Flight(1, "AAA", "10:00", "13/15/2017", "AAA", "11:00", "3/1/2017");
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void whenDepartureDateContainsDaysOver31ThrowIllegalArgumentException() {
+    Flight flight = new Flight(1, "AAA", "10:00", "3/32/2017", "AAA", "11:00", "12/3/2017");
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void whenDepartureDateContainsTooManyDigitsForDayThrowIllegalArgumentException() {
+    Flight flight = new Flight(1, "AAA", "10:00", "3/155/2017", "AAA", "11:00", "3/10/2017");
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void whenDepartureDateContainsTooManyDigitsForYearThrowIllegalArgumentException() {
+    Flight flight = new Flight(1, "AAA", "10:00", "3/15/20177", "AAA", "11:00", "3/10/2017");
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void whenDepartureDateContainsCharactersForMonthThrowException() {
+    Flight flight = new Flight(1, "AAA", "10:00", "Feb/15/2017", "AAA", "11:00", "3/10/20177");
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void whenDepartureTimeDoesNotContainColonThrowIllegalArgumentException() {
+    Flight flight = new Flight(1, "AAA", "1000", "3/15/2017", "AAA", "11:00", "3/15/2017");
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void whenDepartureTimeContainsTooManyDigitsForHourThrowException() {
+    Flight flight = new Flight(1, "AAA", "100:00", "3/15/2017", "AAA", "11:00", "3/15/2017");
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void whenDepartureTimeDoesNotContainHoursThrowException() {
+    Flight flight = new Flight(1, "AAA", ":00", "3/15/2017", "AAA", "11:00", "3/15/2017");
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void whenDepartureTimeDoesNotContainMinsThrowException() {
+    Flight flight = new Flight(1, "AAA", "10:", "3/15/2017", "AAA", "11:00", "3/15/2017");
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void whenDepartureTimeHasHoursGreaterThan23ThrowIllegalArgumentException() {
+    Flight flight = new Flight(1, "AAA", "24:00", "3/15/2017", "AAA", "11:00", "3/15/2017");
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void whenDepartureTimeHasMinsGreaterThan59ThrowIllegalArgumentException() {
+    Flight flight = new Flight(1, "AAA", "23:61", "3/15/2017", "AAA", "23:00", "3/15/2017");
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void whenDepartureTimeHasNeagtiveHoursThrowIllegalArgumentException() {
+    Flight flight = new Flight(1, "AAA", "-10:00", "3/15/2017", "AAA", "10:00", "3/15/2017");
+  }
 }
