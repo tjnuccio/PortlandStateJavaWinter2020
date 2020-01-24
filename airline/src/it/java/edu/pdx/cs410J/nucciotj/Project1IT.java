f094c308df1b00514e87856069b2c56d62a3d232
tjnuccio <tj_nuccio@yahoo.com>
2020-01-23 16:43:40 -0800

    Finished project 1

--- airline/src/it/java/edu/pdx/cs410J/nucciotj/Project1IT.java
+++ airline/src/it/java/edu/pdx/cs410J/nucciotj/Project1IT.java
@@ -92,10 +92,9 @@ public class Project1IT extends InvokeMainTestCase {
         assertThat(result.getTextWrittenToStandardError(), containsString("AA1 contains integer values."));
     }
 
-    @Ignore
     @Test
     public void runMainWithCorrectlyFormattedArgumentsAndCheckStandardOutIsCorrect() {
-        MainMethodResult result = invokeMain(Project1.class,"Airline", "1", "AAA", "3/15/2017", "10:00",  "AAA",  "3/15/2017", "11:00");
+        MainMethodResult result = invokeMain(Project1.class,"-print", "Airline", "1", "AAA", "3/15/2017", "10:00",  "AAA",  "3/15/2017", "11:00");
         assertThat(result.getExitCode(), equalTo(0));
         assertThat(result.getTextWrittenToStandardOut(), containsString("Flight 1 departs AAA at Wed Mar 15 10:00:00 PDT 2017 arrives AAA at Wed Mar 15 11:00:00 PDT 2017"));
     }
--- airline/src/main/java/edu/pdx/cs410J/nucciotj/Project1.java
+++ airline/src/main/java/edu/pdx/cs410J/nucciotj/Project1.java
@@ -1,5 +1,8 @@
 package edu.pdx.cs410J.nucciotj;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+
 /**
  * @author TJ Nuccio
  * The main class for the CS410J airline Project
@@ -14,50 +17,75 @@ public class Project1 {
 
   public static void main(String[] args) {
 
+    LinkedList<String> argList = new LinkedList<>(Arrays.asList(args));     //Holds program arguments
+    LinkedList<String> optionList = new LinkedList<>();                     //Holds command line options
 
-    if(args.length != 8) {          //Test cases for when too few command line arguments are supplied
-      if(args.length == 0) {
-        printErrorMessageAndExit("Missing command line arguments");
-      } else if(args.length == 1) {
-        printErrorMessageAndExit("Missing flight number");
-      } else if(args.length == 2) {
-        printErrorMessageAndExit("Missing departure destination");
-      } else if(args.length == 3) {
-        printErrorMessageAndExit("Missing departure date");
-      } else if(args.length == 4) {
-        printErrorMessageAndExit("Missing departure time");
-      } else if(args.length == 5) {
-        printErrorMessageAndExit("Missing arrival destination");
-      } else if(args.length == 6) {
-        printErrorMessageAndExit("Missing arrival date");
-      } else if(args.length == 7) {
-        printErrorMessageAndExit("Missing arrival time");
-      }
-    } else {
-      String flightNumString = args[1];
-      try {
-        int flightNumber = Integer.parseInt(flightNumString);
-        Flight flight = new Flight(flightNumber, args[2], args[4], args[3], args[5], args[7], args[6]);
-        Airline<Flight> airline = new Airline<>(args[0]);
-        airline.addFlight(flight);
-        flight.toString();
-      } catch (NumberFormatException ex) {
-        printErrorMessageAndExit(flightNumString + " is incorrectly formatted. "  + flightNumString + " should be an integer");
-      } catch(IllegalArgumentException ex) {
-        printErrorMessageAndExit(ex.getMessage());
+    for(int i = 0; i < argList.size(); i++) {
+      if(argList.get(i).matches("-[a-zA-Z0-9]*")) {     //Extract options from command line
+        optionList.add(argList.remove(i));                    //Add options to specific list
+        i--;                                                  //Readjust index for removed element
       }
     }
 
+    if (argList.size() < 8) {          //Test cases for when too few command line arguments are supplied
+      if (argList.size() == 0) {
+        printErrorMessageAndExit("Missing command line arguments");
+      } else if (argList.size() == 1) {
+        printErrorMessageAndExit("Missing flight number");
+      } else if (argList.size() == 2) {
+        printErrorMessageAndExit("Missing departure destination");
+      } else if (argList.size() == 3) {
+        printErrorMessageAndExit("Missing departure date");
+      } else if (argList.size() == 4) {
+        printErrorMessageAndExit("Missing departure time");
+      } else if (argList.size() == 5) {
+        printErrorMessageAndExit("Missing arrival destination");
+      } else if (argList.size() == 6) {
+        printErrorMessageAndExit("Missing arrival date");
+      } else if (argList.size() == 7) {
+        printErrorMessageAndExit("Missing arrival time");
+      }
+    } else {
 
+      if(optionList.contains("-README")) {
+        System.out.println("This is a Project 1. The purpose of project 1 is to read in options and arguments from the command line,\n" +
+                            "parse them, construct two objects, and possibly print to standard output if the option is specified. \n" +
+                            "The program accepts two options: -print and -README. The -README option will print the README associated with \n" +
+                            "the program and exit. The -print option will assemble the program objects, and display the data associated with \n" +
+                            "them both. The command line arguments are: int flightNum, String sourceDestination, String departTime, String departDate, \n" +
+                            "String arrivalDestination, String arrivalTime, String arrivalDate. The program will take these arguments, build a flight object,\n" +
+                            "build an airline object, and add the flight to the airline. Data pertaining to both is displayed with the -print option.");
+        System.exit(0);
+      } else {
+        String flightNumString = argList.get(1);
 
+        try {
+          int flightNumber = Integer.parseInt(flightNumString);
 
-    System.exit(0);
+          Airline<Flight> airline = new Airline<>(argList.get(0));
+          Flight flight = new Flight(flightNumber, argList.get(2), argList.get(4), argList.get(3), argList.get(5), argList.get(7), argList.get(6));
 
+          airline.addFlight(flight);
+
+          if(optionList.contains("-print")) {
+            System.out.println(flight.toString());
+          }
+
+        } catch (NumberFormatException ex) {
+          printErrorMessageAndExit(flightNumString + " is incorrectly formatted. " + flightNumString + " should be an integer");
+
+        } catch (IllegalArgumentException ex) {
+          printErrorMessageAndExit(ex.getMessage());
+        }
+      }
+
+      System.exit(0);
+    }
   }
 
   public static void printErrorMessageAndExit(String msg) {
     System.err.println(msg);
     System.exit(1);
   }
-
 }
+
